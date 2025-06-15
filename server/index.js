caa775599eca81e6f08817af3b9bf52586ee3df4
Sayan Snigdha Pal <sayanpalb@gmail.com>
2025-06-15 13:02:16 +0530

    server port changed

--- server/index.js
+++ server/index.js
@@ -29,5 +29,5 @@ app.get("/", (req, res) => {
   res.send("DocBook API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
